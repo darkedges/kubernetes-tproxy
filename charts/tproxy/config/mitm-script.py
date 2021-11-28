9f07f287e995fbffcf8a9b9c4eb2975d9e5c8372
Anton Horodchuk <antoman1994@gmail.com>
2021-11-28 16:11:03 +0200

    Updated sidecar configmap

--- charts/tproxy/config/mitm-script.py
+++ charts/tproxy/config/mitm-script.py
@@ -3,24 +3,21 @@ import os
 import requests as requests
 from mitmproxy.http import HTTPFlow
 from mitmproxy.script import concurrent
-from urllib.parse import urljoin
 from mitmproxy import ctx
 
-REMOTE_SERVER = os.getenv('REMOTE_SERVER')
-REMOTE_ENDPOINT = '/api/v1.0/interceptor'
-
-INTERCEPT_ENDPOINT = urljoin(REMOTE_SERVER, REMOTE_ENDPOINT)
 
 class ExternalLogger:
 
-    def __init__(self):
-        pass
+    remote_server: str = None
+
+    def __init__(self, remote_server):
+        self.remote_server = remote_server
 
     @concurrent
     def request(self, flow: HTTPFlow) -> None:
         ctx.log.info("Request to %s" % flow.request.pretty_url)
 
-        requests.post(INTERCEPT_ENDPOINT, json={
+        requests.post(self.remote_server, json={
             'url': flow.request.pretty_url,
             'request': flow.request,
             'error': flow.error
@@ -30,8 +27,11 @@ class ExternalLogger:
     def response(self, flow: HTTPFlow) -> None:
         ctx.log.info("response from %s" % flow.request.pretty_url)
 
-        requests.post(INTERCEPT_ENDPOINT, json={
+        requests.post(self.remote_server, json={
             'url': flow.request.pretty_url,
             'response': flow.response,
             'error': flow.error
         })
+
+
+addons = [ExternalLogger(os.getenv("REMOTE_SERVER"))]
