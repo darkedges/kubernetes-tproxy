5f147280e0a9825326ecbb4980b5b94dce393a84
Nicholas Irving <nirving.darkedges@darkedges.com>
2022-09-25 09:57:44 +1000

    updated - removed error from mitmproxy script - updated where certificate is located on centos - removed out of date certifcates from config map

--- charts/tproxy/config/mitm-script.py
+++ charts/tproxy/config/mitm-script.py
@@ -8,30 +8,12 @@ from mitmproxy import ctx
 
 class ExternalLogger:
 
-    remote_server: str = None
-
-    def __init__(self, remote_server):
-        self.remote_server = remote_server
-
     @concurrent
     def request(self, flow: HTTPFlow) -> None:
-        ctx.log.info("Request to %s" % flow.request.pretty_url)
-
-        requests.post(self.remote_server, json={
-            'url': flow.request.pretty_url,
-            'request': flow.request,
-            'error': flow.error
-        })
+        ctx.log.debug("Request to %s" % flow.request.pretty_url)
 
     @concurrent
     def response(self, flow: HTTPFlow) -> None:
-        ctx.log.info("response from %s" % flow.request.pretty_url)
+        ctx.log.debug("response from %s" % flow.request.pretty_url)
 
-        requests.post(self.remote_server, json={
-            'url': flow.request.pretty_url,
-            'response': flow.response,
-            'error': flow.error
-        })
-
-
-addons = [ExternalLogger(os.getenv("REMOTE_SERVER"))]
+addons = [ExternalLogger()]
